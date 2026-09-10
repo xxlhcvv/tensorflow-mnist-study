714f01c083279beef0e0c6313d50f49e77e63f65
xxlhcvv <2407978636@qq.com>
2026-09-10 21:10:33 +0800

    1

--- src/9.10.py
+++ src/9.10.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import pandas as pd
 from tensorflow.keras import datasets  # 导入经典数据集
 
-# 加载 MNIST  data
+# 加载 MNIST data
 (x, y), (x_test, y_test) = datasets.mnist.load_data()
 
 print('x:', x.shape, 'y:', y.shape, 'x_test:', x_test.shape, 'y_test:', y_test.shape)
